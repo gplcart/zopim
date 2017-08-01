3980f3a87c4dd055019a02cb2cb1e3bff664f9cf
GplCart <gplcart.software@gmail.com>
2017-08-01 16:53:59 +0300

    Refactoring

--- Zopim.php
+++ Zopim.php
@@ -31,7 +31,6 @@ class Zopim extends Module
      */
     public function hookRouteList(array &$routes)
     {
-        // Module settings page
         $routes['admin/module/settings/zopim'] = array(
             'access' => 'module_edit',
             'handlers' => array(
